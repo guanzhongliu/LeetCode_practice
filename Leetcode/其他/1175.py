7d0009d3549d45b10a475414a2fa7e4ad8db18c2
Septieme <rogerliu00425@gmail.com>
2021-01-20 22:10:27 +0800

    mv dir

---
+++ Leetcode/其他/1175.py
@@ -0,0 +1,25 @@
+'''
+请你帮忙给从 1 到 n 的数设计排列方案，使得所有的「质数」都应该被放在「质数索引」（索引从 1 开始）上；你需要返回可能的方案总数。
+
+让我们一起来回顾一下「质数」：质数一定是大于 1 的，并且不能用两个小于它的正整数的乘积来表示。
+
+由于答案可能会很大，所以请你返回答案 模 mod 10^9 + 7 之后的结果即可
+'''
+
+
+class Solution:
+    def numPrimeArrangements(self, n: int) -> int:
+        prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31,
+                         37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+        pos_prime, ans = 0, 1
+        for num in prime_numbers:
+            if num <= n:
+                pos_prime += 1
+            else:
+                break
+        pos_num = n - pos_prime
+        for i in range(2, pos_prime + 1):
+            ans = (ans * i) % 1000000007
+        for i in range(2, pos_num + 1):
+            ans = (ans * i) % 1000000007
+        return ans
---
+++ Leetcode/其他/1493.py
@@ -0,0 +1,45 @@
+'''
+给你一个二进制数组 nums ，你需要从中删掉一个元素。
+请你在删掉元素的结果数组中，返回最长的且只包含 1 的非空子数组的长度。
+如果不存在这样的子数组，请返回 0 。
+
+策略：遍历，需要考虑全是1的情况
+'''
+from typing import List
+
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        now_len, length  = 0, len(nums)
+        flag = 1
+        lens = []
+        for i in range(length):
+            flag &= nums[i]
+            if nums[i] == 1:
+                now_len += 1
+            if nums[i] == 0 or i == length - 1:
+                lens.append(now_len)
+                now_len = 0
+        for i in range(len(lens) - 1, 0, -1):
+            lens[i] += lens[i-1]
+        return max(lens) - flag
+
+class Solution_slide:
+    def longestSubarray(self, nums: List[int]) -> int:
+        last_len, now_len, length  = 0, 0, len(nums)
+        ans = 0
+        for i in range(length):
+            if nums[i] == 1:
+                now_len += 1
+                last_len += 1
+            if nums[i] == 0:
+                last_len = now_len
+                now_len = 0
+            ans = max(last_len, ans)
+        if ans == length:
+            ans -= 1
+        return ans
+
+solution = Solution_slide()
+print(solution.longestSubarray([1,0,1,0,0,1]))
+            
